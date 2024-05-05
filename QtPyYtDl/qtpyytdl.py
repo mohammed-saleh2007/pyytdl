6b8c8f80c7be39ea9271fed6c04e0a98cf3535a7
Mohammed Saleh <mohammedsalehbtw@gmail.com>
2024-05-05 03:39:11 +0300

    new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/bin/pytube 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/INSTALLER 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/LICENSE 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/METADATA 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/RECORD 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/REQUESTED 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/WHEEL 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/entry_points.txt 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/top_level.txt 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube-15.0.0.dist-info/zip-safe 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/__init__.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/__main__.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/captions.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/cipher.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/cli.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/contrib/__init__.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/contrib/channel.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/contrib/playlist.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/contrib/search.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/exceptions.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/extract.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/helpers.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/innertube.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/itags.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/metadata.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/monostate.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/parser.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/query.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/request.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/streams.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/pytube/version.py 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget-3.2.dist-info/INSTALLER 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget-3.2.dist-info/METADATA 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget-3.2.dist-info/RECORD 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget-3.2.dist-info/REQUESTED 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget-3.2.dist-info/WHEEL 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget-3.2.dist-info/top_level.txt 	new file:   QtPyYtDl/.qtcreator/Python_3_12_3venv/lib/python3.12/site-packages/wget.py 	modified:   QtPyYtDl/qtpyytdl.py

--- QtPyYtDl/qtpyytdl.py
+++ QtPyYtDl/qtpyytdl.py
@@ -1,15 +1,60 @@
-# This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import  QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QMessageBox
+from pytube import YouTube
+
+class InputDialog(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Qt Python YouTube Videos Downloader")
+        layout = QVBoxLayout(self)
+
+        # Create a label
+        self.label = QLabel("URL:")
+        layout.addWidget(self.label)
+
+        # Create a line edit widget
+        self.url_edit = QLineEdit()
+        layout.addWidget(self.url_edit)
+
+        # Create a label
+        self.label = QLabel("Quality:")
+        layout.addWidget(self.label)
+
+        # Create a line edit widget
+        self.reso_edit = QLineEdit()
+        layout.addWidget(self.reso_edit)
+
+        # Create a button
+        self.button = QPushButton("Download Video")
+        layout.addWidget(self.button)
+
+        # Connect button clicked signal to a slot
+        self.button.clicked.connect(self.handle_submit)
+
+    def handle_submit(self):
+        # Get the text entered by the user
+        video_url = self.url_edit.text()
+        video_quality = self.reso_edit.text()
+        yt = YouTube(video_url)
+        video = yt.streams.filter(only_video=True, resolution=video_quality).first()
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        video_with_sound = video.includes_audio_track(audio_stream)
+        video_with_sound.download()
+        self.show_message("Success!", "Your Video Downloaded Successfully on Quality:" + video_quality)
+
+    def show_message(self, title, message):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle(title)
+        msg_box.setText(message)
+        msg_box.exec()
 
 
-class QtPyYtDl(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
+def main():
+    app = QApplication(sys.argv)
+    dialog = InputDialog()
+    dialog.show()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
-    app = QApplication([])
-    window = QtPyYtDl()
-    window.show()
-    sys.exit(app.exec())
+    main()
